1ec76384f384b9db5a88f66f573d1e9fd3e4a259
Devin Hill <hilldev@pdx.edu>
2021-03-09 12:54:42 -0800

    updated threads.cpp function signature to match questions.h and began refactor to return success/failure to main.cpp from table.cpp

--- Program 2 for Programming Systems/table.cpp
+++ Program 2 for Programming Systems/table.cpp
@@ -1,114 +1,112 @@
 #include "table.h"
 
-table::table(){
-
+table::table() {
     index = new list *[NUM];// make a new array with 3 elements
-
-    for(int i=0;i<NUM; i++){
+    for(int i=0;i<NUM; i++) {
         index[i] = NULL;
     }
-    // ref = 0;
     arrIndex = 0;
 }
 
-table::~table(){
-
+table::~table() {
     //deallocates the array
-    for(int i=0; i<NUM;i++){
-        if(index[i]){
+    for(int i=0; i<NUM;i++) {
+        if(index[i]) {
             delete index[i];
             index[i] = NULL;
         }
     }
 
-    if(index){
+    if(index) {
         delete [] index;
     }
 }
 
-int table::edit(){
-    int toedit;
-    cout<<"enter in the question ref to edit"<<endl;
-    cin>>toedit;
+void table::display() {
+    int pick = 0;
+    cout<<"Which type of question do you want to display?"<<endl;
+    cout<<"Enter 1 for parallel programming"<<endl;
+    cout<<"Enter 2 for threads"<<endl;
+    cout<<"Enter 3 for algorithms"<<endl;
+    cin>>pick;
+    cin.ignore(100, '\n');
 
-    for(int i = 0; i<NUM;i++){
-        if(index[i]){
-            index[i]->search(toedit);
-            int catchh = index[i]->delete_item(toedit);
-            if(catchh == 1){
-                pick();
-            }
-        }
+    if(pick == 1 && index[0]) {
+        index[0]->display();
     }
-    return 0;
+    else if(pick == 2 && index[1]) {
+        index[1]->display();
+    }
+    else if(pick == 3 && index[2]) {
+        index[2]->display();
+    }
 }
 
-int table::remove_w(){
-    int del;
-    cout<<"enter in a reference num to delete: "<<endl;
-    cin>>del;
-    cin.ignore();
-    for(int i = 0; i<NUM;i++){
-        if(index[i]){
-            index[i]->delete_item(del);
-        }
-    }
-
-    return 1;
-}
-
-
-
-void table::search(){
-    int ref =0;
-    cout<<"Which reference number do you want to search for"<<endl;
-    cin>>ref;
-    cin.ignore();
-
-    for(int i = 0; i<NUM;i++){
-        if(index[i]){
-            index[i]->search(ref);
-        }
-    }
-
-    return;
-}
-
-void table::pick(){
+void table::pick() {
     int choice;
-    cout<<"what type of question do you want to add?"<<endl;
+    cout<<"What type of question do you want to add?"<<endl;
     cout<<"Enter 1 for parallel programming"<<endl;
     cout<<"Enter 2 for threads"<<endl;
     cout<<"Enter 3 for algorithms"<<endl;
     cin>>choice;
     cin.ignore(100, '\n');
-    if(index[choice-1] == NULL){
+
+    if(index[choice-1] == NULL) {
         index[choice-1] = new list();
     }
-
     index[choice-1]->insert(choice);
 }
 
+int table::search() {
+    int ref = 0;
+    int found = 1;
+    cout<<"Which reference number do you want to search for:"<<endl;
+    cin>>ref;
+    cin.ignore();
 
-void table::display(){
-
-    int pick = 0;
-    // if(!head) return;
-    cout<<"Which type of question do you want to display"<<endl;
-    cout<<"1 for parallel programming"<<endl;
-    cout<<"2 for threads"<<endl;
-    cout<<"3 for algorithms"<<endl;
-
-    cin>>pick;
-    cin.ignore(100, '\n');
-    if(pick == 1 && index[0]){
-        index[0]->display();
+    for(int i = 0; i<NUM;i++) {
+        if(index[i]) {
+            found = index[i]->search(ref);
+        }
     }
-    else if(pick == 2 && index[1]){
-        index[1]->display();
-    }
-    else if(pick == 3 && index[2]){
-        index[2]->display();
-    }
-
+    return found; //success is 0, failure is 1
 }
+
+int table::remove() {
+    int del;
+    int deleted = 1;
+    cout<<"Enter in a reference num to delete:"<<endl;
+    cin>>del;
+    cin.ignore();
+
+    for(int i = 0; i<NUM;i++) {
+        if(index[i]) {
+            deleted = index[i]->delete_item(del); //catch result
+        }
+    }
+    return deleted; //success is 0, failure is 1
+}
+
+int table::edit() {
+    int toedit;
+    int edited = 1;
+    cout<<"Enter in the question's reference number you want to edit:"<<endl;
+    cin>>toedit;
+
+    for(int i = 0; i<NUM;i++) {
+        if(index[i]) {
+            index[i]->search(toedit);
+            int match = index[i]->delete_item(toedit);
+            if(match == 1) {
+                pick();
+                edited = 0;
+            }
+        }
+    }
+    return edited; //success is 0, failure is 1
+}
+
+
+
+
+
--- Program 2 for Programming Systems/threads.h
+++ Program 2 for Programming Systems/threads.h
@@ -11,8 +11,8 @@ class threads: public questions
         ~threads();
         threads(const threads & src); //copy for a new obj
         threads(int new_subject, char* new_info, char* new_ds, int new_difficulty, int new_tc, int new_reference); //copies data members 
-        virtual void display();
-        void readin();
-    protected: 
+        void display() const override;
+        void readin() override;
+    private:
         int tc; //how many threads are used;
 };
