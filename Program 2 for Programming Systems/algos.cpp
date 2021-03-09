1c00ef1d084b24f9f3bc4f1b3368df3779eebc53
trg5pdx <trg5@pdx.edu>
2021-03-09 12:57:10 -0800

    removed unnecessary comments, fixed formatting of functions & conditional statements, fixed cout line spacing, changed variable names

--- Program 2 for Programming Systems/algos.cpp
+++ Program 2 for Programming Systems/algos.cpp
@@ -5,12 +5,11 @@ algos::algos(){
     big_o_ = NULL;
 }
 
-algos::algos(int new_subject, char* new_info, char* new_ds, int new_difficulty, char* new_bigO, int new_reference): questions(new_subject, new_info, new_ds, new_difficulty, new_reference)
+algos::algos(int new_subject, char* new_info, char* new_ds, int new_difficulty, char* new_bigO, int new_reference): 
+    questions(new_subject, new_info, new_ds, new_difficulty, new_reference)
 {
-
     big_o_ = new char [strlen(new_bigO)+1];
     strcpy(big_o_, new_bigO);
-
 }
 
 algos::algos(const algos & src){
@@ -19,14 +18,15 @@ algos::algos(const algos & src){
 }
 
 algos::~algos(){
-    if(big_o_) 
+    if (big_o_) {
         delete [] big_o_;
+    }
 }
 
 void algos::display(){
     cout << "For algorithms" << endl;
-    cout << "The question is: " << info <<endl;
-    cout << "The data structure: "<< ds <<endl;
+    cout << "The question is: " << info << endl;
+    cout << "The data structure: "<< ds << endl;
     cout << "The difficulty /5 is: " << difficulty << endl;
     cout << "The big O is" << big_o_ << endl;
     cout << "The reference number is: " << reference << endl;
@@ -34,31 +34,28 @@ void algos::display(){
 
 
 void algos::readin(){
-
-    cout <<"The subject is algorithms"<<endl;
-    cout<<"Enter a number to associate the question with"<<endl;
-    cin>>reference;
+    cout << "The subject is algorithms" << endl;
+    cout << "Enter a number to associate the question with" << endl;
+    cin >> reference;
     cin.ignore();
 
-    cout<<"Enter the question"<<endl;
+    cout << "Enter the question" << endl;
     info = new char[1000];
     cin.get(info,1000, '\n');
     cin.ignore(1000,'\n');
 
-    cout<<"Enter the data structure that would work the best"<<endl;
+    cout << "Enter the data structure that would work the best" << endl;
     ds = new char[1000];
     cin.get(ds, 1000, '\n');
     cin.ignore(1000, '\n');
 
-    cout<<"Enter in the diffculty out of 5"<<endl;
-    cin>>difficulty;
+    cout << "Enter in the diffculty out of 5" << endl;
+    cin >> difficulty;
     cin.ignore();
 
-    cout<<"What is the big_o_ for this algo?"<<endl;
+    cout << "What is the big_o_ for this algo?" << endl;
     big_o_ = new char[1000];
     cin.get(big_o_,1000, '\n');
     cin.ignore(1000, '\n');
-
-
 }
 
--- Program 2 for Programming Systems/list.cpp
+++ Program 2 for Programming Systems/list.cpp
@@ -7,34 +7,28 @@ This file contains the functions for the list class
 */
 
 list::list(){
-
     head_ = NULL;
-
 }
 
 list::~list(){
-
     node* temp = head_;
-    if(head_){
-        while(head_){
-            temp = temp->getNext();
-            delete head_;
-            head_ = temp;
-        }
+    while (head_) {
+        temp = temp->getNext();
+        delete head_;
+        head_ = temp;
     }
 }
 
 
 //inserts the data into a doubly linked list
 void list::insert(int choice){
-
-    if(!head_){
+    if (!head_) {
         head_ = new node;
         head_->setData(choice);
         head_->setNext(NULL);
         head_->setPrev(NULL);
     }
-    else{
+    else {
         node * temp = head_;
         head_ = new node;
         head_->setData(choice);
@@ -42,46 +36,43 @@ void list::insert(int choice){
         temp->setPrev(head_);
         return;
     }
-
-
 }
 
 int list::deleteItem(int del){
-
     return deleteItem(head_, del);
-
 }
 
 int list::deleteItem(node*& head_, int del){
-
-    if(!head_) 
+    if (!head_) {
         return 1;
-    if(head_->getData()->getReference() == del){
+    }
+    if (head_->getData()->getReference() == del) {
 
         node* temp = head_->getNext();
         node* temp2 = head_->getPrev();
         delete head_;
         head_ = temp;
 
-        if(head_){
+        if (head_) {
             head_->getPrev() = temp2;
         }
         return 1;
     }
     return deleteItem(head_->getNext(), del);
-
 }
 
 void list::display(){
-    if(!head_) 
+    if (!head_) {
         return;
+    }
 
     display(head_);
 }
 
 void list::display(node* head_){
-    if(!head_) 
+    if (!head_) {
         return;
+    }
 
     head_->display();        // calls node display
 
@@ -89,19 +80,17 @@ void list::display(node* head_){
 }
 
 void list::search(int ref){
-
     traverse(head_, ref);
 }
 
 int list::traverse(node* head_, int ref){
-
-    if(!head_) 
+    if (!head_)  {
         return 0;
-    if(head_->getData()->getReference() == ref){
+    }
+    if (head_->getData()->getReference() == ref) {
         head_->display();
         return 1;
     }
 
     return traverse(head_->getNext(), ref);
-
 }
